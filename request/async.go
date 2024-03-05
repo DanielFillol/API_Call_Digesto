caa6784d5ff22c3172ece38ebcb5375202b0e6ac
daniel_fillol@hotmail.com <55287657+DanielFillol@users.noreply.github.com>
2024-03-05 17:54:24 -0300

    feat: set coll down for every request feat: modified for APIGEE feat: add caller for other records

--- request/async.go
+++ request/async.go
@@ -4,10 +4,11 @@ import (
 	"CallDigesto/models"
 	"log"
 	"sync"
+	"time"
 )
 
 // AsyncAPIRequest makes API requests asynchronously
-func AsyncAPIRequest(users []models.ReadCsv, numberOfWorkers int, url string, method string, auth string) ([]models.ResponseBody, error) {
+func AsyncAPIRequest(users []models.ReadCsv, numberOfWorkers int, url string, method string, auth string, duration time.Duration) ([]models.ResponseBody, error) {
 	// Create a channel to signal when the goroutines are done processing inputs
 	done := make(chan struct{})
 	defer close(done)
@@ -28,7 +29,7 @@ func AsyncAPIRequest(users []models.ReadCsv, numberOfWorkers int, url string, me
 			// Each worker goroutine consumes inputs from the shared input channel
 			for input := range inputCh {
 				// Make the API request and send the response to the result channel
-				bodyStr, err := APIRequest(url, method, auth, input)
+				bodyStr, err := APIRequest(url, method, auth, input, duration)
 				resultCh <- bodyStr
 				if err != nil {
 					// If there is an error making the API request, print the error
@@ -78,4 +79,58 @@ func StreamInputs(done <-chan struct{}, inputs []models.ReadCsv) <-chan models.R
 		}
 	}()
 	return inputCh
+}
+
+func AsyncAPIRequestOther(users []models.ReadCsv, numberOfWorkers int, url string, method string, auth string, duration time.Duration) ([]models.ResponseBodyOtherRecords, error) {
+	done := make(chan struct{})
+	defer close(done)
+	// Create a channel to receive inputs from
+	inputCh := StreamInputs(done, users)
+
+	// Create a wait group to wait for the worker goroutines to finish
+	var wg sync.WaitGroup
+	wg.Add(numberOfWorkers)
+
+	// Create a channel to receive results from
+	resultCh := make(chan models.ResponseBodyOtherRecords)
+
+	// Spawn worker goroutines to process inputs
+	var errorOnApiRequests error
+	for i := 0; i < numberOfWorkers; i++ {
+		go func() {
+			// Each worker goroutine consumes inputs from the shared input channel
+			for input := range inputCh {
+				// Make the API request and send the response to the result channel
+				bodyStr, err := APIRequestOther(url, method, auth, input, duration)
+				resultCh <- bodyStr
+				if err != nil {
+					// If there is an error making the API request, print the error
+					log.Println(err)
+					errorOnApiRequests = err
+					break
+				}
+			}
+			// When the worker goroutine is done processing inputs, signal the wait group
+			wg.Done()
+		}()
+	}
+
+	// Wait for all worker goroutines to finish processing inputs
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+
+	// Return early on error in any given call on API requests
+	if errorOnApiRequests != nil {
+		return nil, errorOnApiRequests
+	}
+
+	// Collect results from the result channel and return them as a slice
+	var results []models.ResponseBodyOtherRecords
+	for result := range resultCh {
+		results = append(results, result)
+	}
+
+	return results, nil
 }
